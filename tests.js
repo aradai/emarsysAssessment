52d55ba25a847a2996411200403bfd47f925328d
Ábris Rádai <abrisradai@gmail.com>
2023-10-13 22:54:51 +0200

    Compact easy to modify testing

--- tests.js
+++ tests.js
@@ -1,82 +1,37 @@
 import { CalculateDueDate } from "./assessment"
 
-console.log("TESTING")
+console.log("TESTING");
 let success = 0;
 let failed = 0;
 
-const date = new Date();
-
 // Expected Behaviour Test Cases
+const goodTests = [
+    //input date, input hours, expected due date
+    ["2023-10-10T10:00:00", 1, "2023-10-10T11:00:00"],
+    ["2023-10-10T16:30:00", 1, "2023-10-11T09:30:00"],
+    ["2023-10-10T10:00:00", 17, "2023-10-12T11:00:00"],
+    ["2023-10-13T16:00:00", 1, "2023-10-13T17:00:00"],
+    ["2023-10-13T16:10:00", 1, "2023-10-16T09:10:00"],
+    ["2023-10-10T10:00:00", 33, "2023-10-16T11:00:00"],
+    ["2023-10-10T10:00:00", 100, "2023-10-26T14:00:00"]
+]
 
-(() => {
-    if(CalculateDueDate(new Date("2023-10-10T10:00:00"),1)[1].toString() === new Date("2023-10-10T11:00:00").toString()){
-        console.log("✔ Test#1 passed");
-        success++;
-    } else {
-        console.log("❌ Test#1 failed");
-        failed++;
-    }
-})();
+console.log("Expected Behavoiur Testing");
 
-(() => {
-    if(CalculateDueDate(new Date("2023-10-10T16:30:00"),1)[1].toString() === new Date("2023-10-11T09:30:00").toString()){
-        console.log("✔ Test#2 passed");
-        success++;
-    } else {
-        console.log("❌ Test#2 failed");
-        failed++;
-    }
-})();
+for(let i = 0; i < goodTests.length; i++){
 
-(() => {
-    if(CalculateDueDate(new Date("2023-10-10T10:00:00"),17)[1].toString() === new Date("2023-10-12T11:00:00").toString()){
-        console.log("✔ Test#3 passed");
-        success++;
-    } else {
-        console.log("❌ Test#3 failed");
-        failed++;
-    }
-})();
+    ((index = i + 1, inputDate = goodTests[i][0], turnaroundHours = goodTests[i][1], expectedResult = goodTests[i][2]) =>
+    {
+        if(CalculateDueDate( new Date(inputDate), turnaroundHours )[1].toString() === new Date(expectedResult).toString()){
+            console.log(`✔ Test#${index} passed`);
+            success++;
+        } else {
+            console.log(`❌ Test#${index} failed`);
+            failed++;
+        }
+    })();
+}
 
-(() => {
-    if(CalculateDueDate(new Date("2023-10-13T16:00:00"),1)[1].toString() === new Date("2023-10-13T17:00:00").toString()){
-        console.log("✔ Test#4 passed");
-        success++;
-    } else {
-        console.log("❌ Test#4 failed");
-        failed++;
-    }
-})();
-
-(() => {
-    if(CalculateDueDate(new Date("2023-10-13T16:10:00"),1)[1].toString() === new Date("2023-10-16T09:10:00").toString()){
-        console.log("✔ Test#5 passed");
-        success++;
-    } else {
-        console.log("❌ Test#5 failed");
-        failed++;
-    }
-})();
-
-(() => {
-    if(CalculateDueDate(new Date("2023-10-10T10:00:00"),33)[1].toString() === new Date("2023-10-16T11:00:00").toString()){
-        console.log("✔ Test#6 passed");
-        success++;
-    } else {
-        console.log("❌ Test#6 failed");
-        failed++;
-    }
-})();
-
-(() => {
-    if(CalculateDueDate(new Date("2023-10-10T10:00:00"),100)[1].toString() === new Date("2023-10-26T14:00:00").toString()){
-        console.log("✔ Test#7 passed");
-        success++;
-    } else {
-        console.log("❌ Test#7 failed");
-        failed++;
-    }
-})();
 
 console.log(`✔ Successfull: ${success}/${success + failed}`);
 console.log(`❌ Failed: ${failed}/${success + failed}`);
